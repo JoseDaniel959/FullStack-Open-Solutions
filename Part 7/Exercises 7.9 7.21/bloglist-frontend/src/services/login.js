55caf0692d6c7d2901953ae0c006503402475200
José Daniel Bustamante Arango <jobustamantea@unal.edu.co>
2025-01-25 01:58:00 -0500

    se eliminan archivos

--- Part 7/Exercises 7.9 7.21/bloglist-frontend/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const baseURL = "http://localhost:3000/api/login"
-
-const login = async credentials =>{
-    const {data} = await axios.post(baseURL,credentials)
-    return data
-}
-
-export default {login} 
